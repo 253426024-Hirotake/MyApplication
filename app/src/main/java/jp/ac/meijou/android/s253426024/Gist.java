aa7fe7f2223e4a22cbc54ea6ec6f32e38b770786
Hirotake Tomita <hirotake.tomita@ucl.meijo-u.ac.jp>
2025-09-04 12:26:22 +0900

    2限目までの内容をコミット．

---
+++ app/src/main/java/jp/ac/meijou/android/s253426024/Gist.java
@@ -0,0 +1,11 @@
+package jp.ac.meijou.android.s253426024;
+
+import java.util.Map;
+
+public class Gist {
+    public Map<String, GistFile> files;
+
+    public static class GistFile {
+        public String content;
+    }
+}
